6865adec19c0762fab5be21bbb64bb314211bf54
SoyKhaler <aaronstone2000@gmail.com>
2024-01-10 15:24:43 +0100

    delete source matsa_quiz

--- matsa_quiz/ranking.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-var leaderboard = "res://addons/silent_wolf/Scores/Leaderboard.tscn"
-
-func _ready():
-	SilentWolf.Scores.persist_score(Global.player_name, Global.score)
-	yield(get_tree().create_timer(0.3),"timeout")
-	get_tree().change_scene(leaderboard)
-	pass
